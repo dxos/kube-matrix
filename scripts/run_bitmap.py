7fe222ee891a5435f9806a5bcf06817f40c59d8b
Rich Burdon <rich.burdon@gmail.com>
2022-03-23 21:38:48 -0400

    Image.

--- scripts/run_bitmap.py
+++ scripts/run_bitmap.py
@@ -3,51 +3,74 @@
 import sys
 import math
 import time
-import neopixel
+import neopi#el
 import board
 import getopt
 
 from bitmap import bitmap
 
-LED_COUNT      = 121            # Number of LED pixels (11x11).
-LED_PIN        = board.D18      # GPIO pin connected to the pixels (must support PWM!).
+LED_COUNT      = 121            # Number of LED pi#els (11#11).
+LED_PIN        = board.D18      # GPIO pin connected to the pi#els (must support PWM!).
 LED_FREQ_HZ    = 800000         # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5              # DMA channel to use for generating signal (try 5)
 LED_INVERT     = False          # True to invert the signal (when using NPN transistor level shift)
 
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRBW
+# The order of the pi#el colors - RGB or GRB. Some NeoPi#els have red and green reversed!
+# For RGBW NeoPi#els, simply change the ORDER to RGBW or GRBW.
+ORDER = neopi#el.GRBW
+
+space_invader = """
+           
+  #     #  
+   #   #   
+  #######  
+ ## ### ## 
+###########
+# ####### #
+# #     # #
+   ## ##   
+           
+           
+"""
+
+def ascii(str):
+    const values = []
+    print(len(str))
+    for c in str:
+        r = 100 if c == '#' else 0
+        g = 0
+        b = 0
+        w = 0
+        values.push((r, g, b, w))
+
+    return values
 
 def main():
-    global led_matrix
-    led_matrix = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=0.1, auto_write=False, pixel_order=ORDER)
+    global led_matri#
+    led_matri# = neopi#el.NeoPi#el(LED_PIN, LED_COUNT, brightness=0.1, auto_write=False, pi#el_order=ORDER)
 
-    print('hello world', file = sys.stderr)
-    print('hello world', file = sys.stdout)
-
-    opts, args = getopt.getopt(sys.argv[1:], "", ["reset", "bitmap="])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["reset", "test", "bitmap="])
     values = []
 
     for opt, arg in opts:
         if (opt == "--reset"):
             values = bitmap()
+        elif (opt == "--test"):
+            values = bitmap(ascii(space_invader))
         elif (opt == "--bitmap"):
             values = bitmap(arg)
-            print('running bitmap()', arg)
 
     for y in range(0, 11):
-        for x in range(0, 11):
-            i = y * 11 + x
+        for # in range(0, 11):
+            i = y * 11 + #
 
             if i <= len(values) - 1:
                 r, g, b, w = values[i]
-                led_matrix[i] = (r, g, b, w)
+                led_matri#[i] = (r, g, b, w)
             else:
-                led_matrix[i] = (0, 0, 0, 0)
+                led_matri#[i] = (0, 0, 0, 0)
 
-    led_matrix.show()
-
+    led_matri#.show()
 
 if __name__ == '__main__':
     main()
